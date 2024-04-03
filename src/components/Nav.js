dde1fa8c89d60e2fa3de9a0d353f06d9ef5f19a2
KingProsper95 <ajoninajerryprosper@gmail.com>
2024-04-03 17:04:51 +0100

    header modifier

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,17 @@
 import { Link } from "react-router-dom";
 import { HStack } from "@chakra-ui/react";
+import styled from "styled-components";
 
 function Nav(){
     return (
-        <>
-            <HStack as="nav" color='black'>
-                <Link to="/">Home</Link>
-                <Link>About</Link>
-                <Link>Menu</Link>
-                <Link to="/reservations">Reservations</Link>
-                <Link>Order online</Link>
-                <Link>Login</Link>
-            </HStack>
-        </>
+        <HStack as="nav">
+            <Link to="/">Home</Link>
+            <Link>About</Link>
+            <Link>Menu</Link>
+            <Link to="/reservations">Reservations</Link>
+            <Link>Order online</Link>
+            <Link>Login</Link> 
+        </HStack>   
     )
 }
 
